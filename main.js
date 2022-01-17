924b3842ee9f01cbd605bd4f752f13b61b3c411a
Nashke <nahashon.kamau@student.moringaschool.com>
2022-01-17 10:52:59 +0300

    add fucntion to purchase

--- main.js
+++ main.js
@@ -12,4 +12,9 @@ var purchase = document.getElementsByClassName('btn-primary');
 for( i = 0 ; i < removebtn.length ; i++){
     removebtn[i].addEventListener('click' , removebtn)
 }
-
+function purchase(event){
+    var purchase = event.target;
+    var purchaseE1 = purchase.parentElement
+    console.log(purchaseE1)
+    var imgname = purchaseE1.children[0].src;
+}
